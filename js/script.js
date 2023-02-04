4f19c391ddda3840caf9a7ff4b787692e7ba925f
Palubski Maciej <palubskimaciej@gmail.com>
2023-02-04 20:19:01 +0100

    Ternary and Navigation added

--- js/script.js
+++ js/script.js
@@ -3,18 +3,15 @@ console.log("Hello World!");
 
 const button = document.querySelector(".js-button");
 const body = document.querySelector(".js-body");
-const theme = document.querySelector(".theme");
+const themeName = document.querySelector(".theme");
 
 
 
 button.addEventListener("click", () => {
     body.classList.toggle("body--dark");
 
-    if (body.classList.contains("body--dark")) {
-        theme.innerText = "jasny";
-    } else {
-        theme.innerText = "ciemny";
-    }
+   themeName.innerText = body.classList.contains("body--dark") ? "jasny" : "ciemny";  
+
 });
 
 
@@ -25,10 +22,8 @@ const pictureToggle = document.querySelector(".pictureToggle");
 pictureButton.addEventListener("click", () => {
     picture.classList.toggle("js-noImage");
 
-    if (picture.classList.contains("js-noImage")) {
-        pictureToggle.innerText = "Pokaż";
-    } else {
-        pictureToggle.innerText = "Ukryj";
-    }
+    pictureToggle.innerText = picture.classList.contains("js-noImage") ? "Pokaż" : "Ukryj";
+
+    
 });
 
